183f6cc9f45e2f77babb62c4bccc0d2157a2330b
Mikael <mikael@devkinetics.com>
2023-02-22 23:11:29 +0800

    Disable other master_arn

--- role_mapping.tf
+++ role_mapping.tf
@@ -13,17 +13,17 @@ resource "elasticsearch_opensearch_roles_mapping" "role_mapping" {
   depends_on = [elasticsearch_opensearch_role.role]
 }
 
-resource "elasticsearch_opensearch_roles_mapping" "master_user_arn" {
-  for_each = {
-    for key in ["all_access", "security_manager"] :
-    key => try(local.role_mappings[key], {})
-  }
+# resource "elasticsearch_opensearch_roles_mapping" "master_user_arn" {
+#   for_each = {
+#     for key in ["all_access", "security_manager"] :
+#     key => try(local.role_mappings[key], {})
+#   }
 
-  role_name     = each.key
-  description   = try(each.value.description, "")
-  backend_roles = concat(try(each.value.backend_roles, []), [var.master_user_arn])
-  hosts         = try(each.value.hosts, [])
-  users         = try(each.value.users, [])
+#   role_name     = each.key
+#   description   = try(each.value.description, "")
+#   backend_roles = concat(try(each.value.backend_roles, []), [var.master_user_arn])
+#   hosts         = try(each.value.hosts, [])
+#   users         = try(each.value.users, [])
 
-  depends_on = [aws_route53_record.opensearch]
-}
+#   depends_on = [aws_route53_record.opensearch]
+# }
